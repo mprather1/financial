d810909656cd92fd9cfa324a40b4295bf88812b7
Michael Prather <mprather1@gmail.com>
2016-07-16 20:30:48 -0500

    account

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'accounts#index'
   resources :accounts
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/migrate/20160717010734_create_accounts.rb
+++ db/migrate/20160717010734_create_accounts.rb
@@ -1,6 +1,8 @@
 class CreateAccounts < ActiveRecord::Migration
   def change
     create_table :accounts do |t|
+      t.string :description
+      t.float :balance
 
       t.timestamps null: false
     end
